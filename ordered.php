0db1081dd19d5f931b1ed1cad431f76cb8ff2ec2
yutoun <yutounwasese@gmail.com>
2020-01-10 07:48:57 +0900

    done

--- ordered.php
+++ ordered.php
@@ -11,20 +11,25 @@
     <div class="main">
       <h2 class="nexttitle">注文確認</h2>
       <?php $totalPayment= 0;?>
+      <!-- これ外にしないと配列回す度にtotalpaymentが0になっちゃう -->
       <?php foreach ($menus as $menu): ?>
         <!-- ↘︎nameをつけたinputの数字がプロパティとして出力される -->
         <?php
           $amount = $_POST[$menu->getName()] ;
           $menu -> setAmount($amount);
+          // ???
           $totalPayment += $menu->getTotalPrice();
         ?>
-        <p>
+        <p class="amount">
           <?php echo $menu->getName() ?>
-          ×
           <?php echo $amount ?>
-          = ￥
+          個
+        </p>
+        <p>
+          ￥
           <?php echo $menu->getTotalPrice()?>
         </p>
+        <br>
           <!-- postがないと何個とったかがわからない -->
       <?php endforeach?>
       <p>合計金額:￥<?php echo $totalPayment ?></p>
